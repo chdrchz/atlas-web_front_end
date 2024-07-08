a2f25dde8cbb7ac4ecafac2e6eb5726aaf8f6a9b
chdrchz <saavannaa.rose@gmail.com>
2024-07-08 18:46:42 -0500

    using performance api to calculate total time for function calls

---
+++ Javascript_advanced/10-prime.js
@@ -0,0 +1,29 @@
+function countPrimeNumbers() {
+    let primeCount = 0;
+
+    for (let i = 2; i < 100; i++) {
+        let isPrime = true;
+        
+        for (let j = 2; j <= i / 2; j++) {
+            if (i % j === 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        
+        if (isPrime) {
+            primeCount++;
+        }
+    }
+
+    return primeCount;
+}
+
+let startTime = performance.now();
+for (let i = 0; i < 100; i++) {
+    countPrimeNumbers();
+}
+let endTime = performance.now();
+let totalTime = endTime - startTime;
+
+console.log(`Execution time of printing countPrimeNumbers was ${totalTime} milliseconds`);
