9b18f42ef39285b318a96467f6b383d6f0d68431
chdrchz <saavannaa.rose@gmail.com>
2024-07-12 12:24:32 -0500

    adding expiration dates for cookies

--- Cookies_local_storage/src/index.js
+++ Cookies_local_storage/src/index.js
@@ -7,15 +7,20 @@ function setCookies() {
   var firstNameValue = $("#firstname").val();
   var emailValue = $("#email").val();
 
+  // Expiration date
+  var expirationDate = new Date();
+  expirationDate.setDate(expirationDate.getDate() + 30); // Expires in 30 days
+  var expires = "expires=" + expirationDate.toUTCString() + "; path=/";
+
+  // Set or overwrite cookies
   if (firstNameValue) {
-    document.cookie =
-      "firstname=" + encodeURIComponent(firstNameValue) + "; path=/";
+    document.cookie = "firstname=" + encodeURIComponent(firstNameValue) + "; " + expires;
   } else {
     console.log("No value entered in the firstname input field.");
   }
 
   if (emailValue) {
-    document.cookie = "email=" + encodeURIComponent(emailValue) + "; path=/";
+    document.cookie = "email=" + encodeURIComponent(emailValue) + "; " + expires;
   } else {
     console.log("No value entered in the email input field.");
   }
