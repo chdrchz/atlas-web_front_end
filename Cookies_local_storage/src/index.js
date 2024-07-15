18cfa7ee3902bf88d8ed28713f646c9057856505
chdrchz <saavannaa.rose@gmail.com>
2024-07-15 10:41:53 -0500

    fixing environment to be in development mode AND finishing up task 2 by adding a getCookie function based on the string passed in. Ie, email or firstname

--- Cookies_local_storage/src/index.js
+++ Cookies_local_storage/src/index.js
@@ -26,7 +26,26 @@ function setCookies() {
   }
 }
 
+function getCookie(name) {
+  var cookies = document.cookie.split(';'); // Splits cookies into an array
+
+  // Iterate over array and decode the cookie
+  for (var i = 0; i < cookies.length; i++) {
+
+    // Trim whitespace and split name= and name. ie: name=Savanna will be 'name', 'savanna'
+    var cookiePair = cookies[i].trim().split('=');
+
+    // If the key equals name, then decode the corresponding value to get yo cookie
+    if (decodeURIComponent(cookiePair[0]) === name) {
+      return decodeURIComponent(cookiePair[1]);
+    }
+  }
+  // No cookie was found, return empty string
+  return "";
+}
+
 function showCookies() {
-  var cookies = $("<p>").html(`<p>Cookies: ${document.cookie}</p>`);
-  $("body").append(cookies);
+  var email = getCookie("email");
+  var firstName = getCookie("firstname");
+  $('body').append(`<p>Email: ${email} - Firstname: ${firstName}</p>`);
 }
